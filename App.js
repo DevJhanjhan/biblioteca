2185ebf9872d5335e195c37bc5510619873d0f1c
Jhanett Vivian <152014155+DevJhanjhan@users.noreply.github.com>
2024-02-29 17:25:12 -0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,19 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+import BottomTabNavigator from './components/BottomTabNavigator' ;
+
+
+export default function App() {
+  return (
+    <BottomTabNavigator/>
+  )
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
